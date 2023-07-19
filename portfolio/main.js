e9becc824a77e4df0d2c2a72118f3f8dbf8ae4b5
martin-mcinerney <martin_mcinerney@live.ie>
2023-07-19 10:44:04 +0100

    Adds lighting and orbit controls

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,13 +1,20 @@
 import './style.css'
+import {
+  OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls'
 
 import * as THREE from 'three';
+import {
+  AmbientLight,
+  PointLight
+} from 'three';
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg"),
+  canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -17,17 +24,36 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const gemoetry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff6347,
-  wireframe: true
+const material = new THREE.MeshStandardMaterial({
+  color: 0xff6347
 });
 
 const torus = new THREE.Mesh(gemoetry, material);
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(ambientLight)
+scene.add(pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper);
+
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
 function animate() {
   requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.y += 0.01;
+  controls.update()
   renderer.render(scene, camera);
 }
 
